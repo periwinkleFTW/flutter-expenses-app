6bba7c27b4961bc61a17b84f7002d5057796a36f
periwinkleFTW <29137960+periwinkleFTW@users.noreply.github.com>
2022-02-04 01:12:43 -0700

    adaptive flat button

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:intl/intl.dart';
 
+import './adaptive_flat_button.dart';
+
 class NewTransaction extends StatefulWidget {
   final Function addTx;
 
@@ -96,17 +98,7 @@ class _NewTransactionState extends State<NewTransaction> {
                             : 'Picked Date: ${DateFormat.yMd().format(_selectedDate)}',
                       ),
                     ),
-                    Platform.isIOS
-                        ? CupertinoButton(
-                            child: Text('Choose Date...',
-                                style: TextStyle(fontWeight: FontWeight.bold)),
-                            onPressed: _presentDatePicker,
-                          )
-                        : FlatButton(
-                            onPressed: _presentDatePicker,
-                            textColor: Theme.of(context).primaryColor,
-                            child: Text('Choose Date...',
-                                style: TextStyle(fontWeight: FontWeight.bold)))
+                    AdaptiveFlatButton('Choose Date', _presentDatePicker),
                   ],
                 ),
               ),
